29106b72c6184e443dfe35a97ee7624501337168
David Smith <codeactual@gmail.com>
2013-05-07 14:49:45 -0700

    chore(dist): Regenerate

--- dist/sinon-doublist-fs.js
+++ dist/sinon-doublist-fs.js
@@ -162,6 +162,35 @@
             return obj;
         };
     });
+    require.register("component-type/index.js", function(exports, require, module) {
+        var toString = Object.prototype.toString;
+        module.exports = function(val) {
+            switch (toString.call(val)) {
+              case "[object Function]":
+                return "function";
+
+              case "[object Date]":
+                return "date";
+
+              case "[object RegExp]":
+                return "regexp";
+
+              case "[object Arguments]":
+                return "arguments";
+
+              case "[object Array]":
+                return "array";
+
+              case "[object String]":
+                return "string";
+            }
+            if (val === null) return "null";
+            if (val === undefined) return "undefined";
+            if (val && val.nodeType === 1) return "element";
+            if (val === Object(val)) return "object";
+            return typeof val;
+        };
+    });
     require.register("component-clone/index.js", function(exports, require, module) {
         var type;
         try {
@@ -202,35 +231,6 @@
                 return obj;
             }
         }
-    });
-    require.register("component-type/index.js", function(exports, require, module) {
-        var toString = Object.prototype.toString;
-        module.exports = function(val) {
-            switch (toString.call(val)) {
-              case "[object Function]":
-                return "function";
-
-              case "[object Date]":
-                return "date";
-
-              case "[object RegExp]":
-                return "regexp";
-
-              case "[object Arguments]":
-                return "arguments";
-
-              case "[object Array]":
-                return "array";
-
-              case "[object String]":
-                return "string";
-            }
-            if (val === null) return "null";
-            if (val === undefined) return "undefined";
-            if (val && val.nodeType === 1) return "element";
-            if (val === Object(val)) return "object";
-            return typeof val;
-        };
     });
     require.register("component-each/index.js", function(exports, require, module) {
         var type = require("type");
