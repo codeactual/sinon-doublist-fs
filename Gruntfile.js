e673fa228f3d5b0b6b437394334093f7d6414134
David Smith <codeactual@gmail.com>
2013-06-02 15:28:25 -0700

    chore(grunt-horde): Upgrade to 0.3.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,9 @@ module.exports = function(grunt) {
 
   require('grunt-horde')
     .create(grunt)
-    .demand('projName', 'sinon-doublist-fs')
-    .demand('instanceName', 'sinonDoublistFs')
-    .demand('klassName', 'sinonDoublistFs')
+    .demand('initConfig.projName', 'sinon-doublist-fs')
+    .demand('initConfig.instanceName', 'sinonDoublistFs')
+    .demand('initConfig.klassName', 'sinonDoublistFs')
     .loot('node-component-grunt')
     .attack();
 };
